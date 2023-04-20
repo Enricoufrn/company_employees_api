9badae688bee4d95385d20c0705a924d8ba1ad89
Enrico <enricoluigi1098@gmail.com>
2023-04-20 19:14:20 -0300

    Primeiro commit - cap 1

---
+++ CompanyEmployees/Extensions/ServicesExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace CompanyEmployees.Extensions;
+
+public static class ServicesExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
+    {
+        options.AddPolicy("CorsPolicy", builder =>
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+    });
+
+    public static void ConfigureISSIntegration(this IServiceCollection services) =>
+        services.Configure<IISOptions>(options =>
+        {
+
+        });
+}
